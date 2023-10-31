6e18166dfc7f90c30996baffe4f026bf8da9578d
Doan Quang Huy <quanghuybest@gmail.com>
2023-10-31 15:32:09 +0700

    them say test

---
+++ src/say/Bye.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace quanghuybest2k2\abc\say;
+
+class Bye
+{
+    public function sayBye(string $ten): string
+    {
+        return "Goodbye " . $ten;
+    }
+}
---
+++ src/say/Hello.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace quanghuybest2k2\abc\say;
+
+class Hello
+{
+    public function sayHello(string $ten): string
+    {
+        return "Hello " . $ten;
+    }
+}
